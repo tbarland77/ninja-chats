55b7c302ad68c06c25dc85b64d0b346ecd06768a
Timothy Barland <tbarland77@hotmail.com>
2018-08-23 19:24:55 -0400

    added scrollbar to chat window

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Chat from '@/components/Chat'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -27,20 +28,3 @@ export default new Router({
     }
   ]
 })
-
-// {
-//   path: '/chat',
-//   name: 'Chat',
-//   component: Chat,
-//   props: true,
-//   beforeEnter: (to, from, next) => {
-//     console.log(to.params)
-//     if(to.params.name){
-//       next()
-//     } else {
-//       next({ name: 'Welcome' })
-//     }
-//   }
-// }
-// ]
-// })
